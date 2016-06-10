cbe7417fb866bdbac70fdf66ccc0b4dfbb22b13e
openshift.nodejsTest <openshift.nodejs@yandex.ru>
2016-06-10 23:55:55 +0400

    addЭ

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,14 @@ module.exports = function(app) {
 	console.log("/");
 	res.send('Hello world!');
     })
+    app.get('/ping', function(req ,res){
+	 res.json({
+               code : "0",
+               answer :"empty field",
+               data : [],
+	});
+
+    });
     app.post('/enter', require('./enter').post);
     app.post('/startgame', require('./startgame').post);
 
